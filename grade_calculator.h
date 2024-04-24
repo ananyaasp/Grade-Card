a1df200991f23cf4c0648175ce3376ed32b76b1d
ananyaasp <154671538+ananyaasp@users.noreply.github.com>
2024-04-24 19:33:40 +0530

    Delete grade_calculator.h

--- grade_calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRADE_CALCULATOR_H
-#define GRADE_CALCULATOR_H
-
-void calculate_grade_card(char *name, int marks[][3]);
-char calculate_grade(float marks);
-float calculate_points(int marks);
-const char *get_subject_name(int subject_num);
-
-#endif
